796288f47f23bf32f7fe7bba481d0947e514efa1
meq <fewell@gmail.com>
2017-11-19 16:32:45 -0500

    Working on courses CRUD

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -8,9 +8,43 @@ class CoursesController < ApplicationController
   def show
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def edit
+  end
+
+  def create
+    #raise params.inspect
+    @course = Course.new(course_params)
+
+    respond_to do |format|
+      if @course.save
+        format.html { redirect_to @course, notice: 'Coure was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @course }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @course.destroy
+    respond_to do |format|
+      format.html { redirect_to courses_url }
+      format.json { head :no_content }
+    end
+  end
+
 private
   def set_course
     @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :location, :credits, :learning_objective_1, :learning_objective_2, :learning_objective_3, :start_date, :end_date, :published, :instructor_id => [])
   end
 
 end
