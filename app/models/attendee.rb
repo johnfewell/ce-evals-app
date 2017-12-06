dc235901e117f844511ffd3d01bc748bfaef78ac
meq <fewell@gmail.com>
2017-12-05 19:54:00 -0500

    Got the incomplete evals scope method working for Attendees

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -9,6 +9,22 @@ class Attendee < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def incomplete_evaluations
+    completed_courses = []
+    incomplete_evaluations_array =[]
+    self.courses.each do |c|
+      if c.complete?
+        completed_courses << c
+      end
+    end
+    completed_courses.each do |cc|
+      if cc.attendees.find(self.id)
+        incomplete_evaluations_array << cc
+      end
+    end
+    incomplete_evaluations_array
+  end
+
   def self.import(file, course_id)
     CSV.foreach(file.path, headers: true) do |row|
       attendee = Attendee.new row.to_hash
