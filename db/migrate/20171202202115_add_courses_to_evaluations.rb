cb04fbe6080d9c5f357fcc9fac35c8425e74dbdd
meq <fewell@gmail.com>
2018-02-22 12:40:39 -0500

    edited migrations that were causing migration to fail

--- db/migrate/20171202202115_add_courses_to_evaluations.rb
+++ db/migrate/20171202202115_add_courses_to_evaluations.rb
@@ -1,4 +1,4 @@
-class AddCourseToEvaluations < ActiveRecord::Migration[5.1]
+class AddCoursesToEvaluations < ActiveRecord::Migration[5.1]
   def change
     add_column :evaluations, :course_id, :integer
   end
